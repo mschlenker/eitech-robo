84bb9d5f0a1476d3ef767031c7aaa8b299473f46
Mattias Schlenker <ms@mattiasschlenker.de>
2018-11-02 15:43:58 +0100

    add arduino code

--- misc/text2hex.rb
+++ misc/text2hex.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/ruby
 
+puts "static const unsigned char index_html[] = {"
+
 File.open(ARGV[0]).each { |line|
-	unless line.strip =~ /^\/\// 
+	unless line.strip =~ /^\/\// || line.strip == ""
 		(line + "\r").chars { |c|
 			print "0x%02x, " % [ c.ord ]
 		}
 		print "\n"
 	end
 }
+
+puts "};"
